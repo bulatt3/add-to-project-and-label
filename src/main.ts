9e72569d661cdb4950b4da73068cbf9e2628e414
Olga Bulat <obulat@gmail.com>
2023-04-05 10:18:16 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,11 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {addToProject} from './add-to-project'
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
-}
-
-run()
+addToProject()
+  .catch(err => {
+    core.setFailed(err.message)
+    process.exit(1)
+  })
+  .then(() => {
+    process.exit(0)
+  })
